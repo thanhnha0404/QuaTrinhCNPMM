b31b5fda3f3e4ca23581d5b9a9f92ee5a9a6b10a
Thanh Nha <thanhnha04042004@gmail.com>
2025-09-09 15:35:54 +0700

    search and filter

--- BT29_08/FullStackNodeJS01/ExpressJS01/src/config/viewEngine.js
+++ BT29_08/FullStackNodeJS01/ExpressJS01/src/config/viewEngine.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 
+
 const configViewEngine = (app) => {
     app.set('views', path.join('./src', 'views'));
     app.set('view engine', 'ejs');
